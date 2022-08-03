868832ccf607c6a52d0904ad0e68745eea4f50fd
Kan Adachi <kadachi1023@gmail.com>
2022-08-03 16:07:43 -0700

    Adds clarifying comment to `solanaPublicKeys`

--- src/types/user-wallet.ts
+++ src/types/user-wallet.ts
@@ -1,3 +1,8 @@
 export interface FractalUserWallet {
+  /**
+   * The list of solana public keys belonging to this user wallet. Currently, we
+   * only support one solana address per fractal wallet, but this may change in
+   * the future (hence a list).
+   */
   solanaPublicKeys: string[];
 }
