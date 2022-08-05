5b9ecfb7c70ab71e97ea2a2c700ad4b0adde998b
Kan Adachi <kadachi1023@gmail.com>
2022-08-05 08:34:57 -0700

    Exports the scope as an enum instead of a type

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ export { SignIn as SignInWithFractal } from 'components/sign-in';
 export { UserContextProvider as SignInWithFractalProvider } from 'context/user';
 export type { SignInProps as SignInWithFractalProps } from 'components/sign-in';
 
+export { Scope as FractalSdkScope } from 'types';
 export type {
   FractalUser,
   FractalUserWallet,
   FractalCoin,
   FractalItem,
-  Scope as FractalSdkScope,
 } from 'types';
 
 export { useFractalUser } from 'hooks/use-fractal-user';
