eacdc6a79aa7ec5a375a8fb75f19383a16d98516
Kan Adachi <kadachi1023@gmail.com>
2022-08-08 14:04:22 -0700

    Changes button styles

--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -2,14 +2,14 @@ import { css } from '@emotion/css';
 import { Logo } from 'components/logo';
 import { HTMLAttributes } from 'react';
 
-const DEFAULT_BUTTON_TEXT = 'Sign in with';
+const DEFAULT_BUTTON_TEXT = 'Sign in with Fractal';
 
 export const SignInButton = (
   buttonProps: HTMLAttributes<HTMLButtonElement>,
 ) => {
   const className = [
     css(`
-      align-items: baseline;
+      align-items: center;
       background: #f2059f;
       border-radius: 0.25rem;
       border: 0;
@@ -17,10 +17,8 @@ export const SignInButton = (
       cursor: pointer;
       display: flex;
       font-size: 1rem;
-      font-weight: 500;
-      gap: 0.375em;
-      justify-content: center;
-      padding: 0.75rem 1rem;
+      gap: 0.625rem;
+      padding: 0.125rem 0.75rem 0.125rem 0.125rem;
       width: max-content;
 
       &:hover {
@@ -31,8 +29,22 @@ export const SignInButton = (
   ]
     .join(' ')
     .trim();
+
   return (
     <button {...buttonProps} className={className}>
+      <div
+        className={css`
+          background: #fff;
+          width: 2rem;
+          aspect-ratio: 1/1;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 1.5px;
+        `}
+      >
+        <Logo></Logo>
+      </div>
       <span
         className={css`
           white-space: nowrap;
@@ -40,7 +52,6 @@ export const SignInButton = (
       >
         {DEFAULT_BUTTON_TEXT}
       </span>
-      <Logo />
     </button>
   );
 };
