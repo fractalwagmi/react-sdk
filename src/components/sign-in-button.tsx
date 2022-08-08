fe0cacf20c23ae48e34e1e78f4a903cd8cbba3f6
Kan Adachi <kadachi1023@gmail.com>
2022-08-08 08:50:42 -0700

    Adds fractal logo

--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -1,31 +1,46 @@
 import { css } from '@emotion/css';
+import { Logo } from 'components/logo';
 import { HTMLAttributes } from 'react';
 
-export const DEFAULT_BUTTON_TEXT = 'Sign in with Fractal';
+const DEFAULT_BUTTON_TEXT = 'Sign in with';
 
 export const SignInButton = (
   buttonProps: HTMLAttributes<HTMLButtonElement>,
 ) => {
   const className = [
-    css`
+    css(`
+      align-items: baseline;
+      background: #f2059f;
+      border-radius: 0.25rem;
+      border: 0;
+      color: #fff;
+      cursor: pointer;
+      display: flex;
+      font-size: 1rem;
+      font-weight: 500;
+      gap: 0.375em;
+      justify-content: center;
+      padding: 0.75rem 1rem;
+      width: max-content;
+
       &:hover {
         background: #ff3dbb;
       }
-      cursor: pointer;
-      border: 0;
-      background: #f2059f;
-      border-radius: 4px;
-      color: #fff;
-      padding: 0.75rem 1rem;
-      font-size: 1rem;
-    `,
+    `),
     buttonProps.className ?? '',
   ]
     .join(' ')
     .trim();
   return (
     <button {...buttonProps} className={className}>
-      {DEFAULT_BUTTON_TEXT}
+      <span
+        className={css`
+          white-space: nowrap;
+        `}
+      >
+        {DEFAULT_BUTTON_TEXT}
+      </span>
+      <Logo />
     </button>
   );
 };
