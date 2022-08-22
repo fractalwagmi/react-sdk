67ac786f4ff06ad3f762e9e49724f3255803ecaf
Kan Adachi <kadachi1023@gmail.com>
2022-08-22 06:43:42 -0700

    Ensures new scopes dont fall through `isValidScope`

--- src/core/scope.ts
+++ src/core/scope.ts
@@ -4,13 +4,6 @@ export function verifyScopes(scopes: string[]): boolean {
   return scopes.every((maybeScope: string) => isValidScope(maybeScope));
 }
 
-function isValidScope(maybeScope: string) {
-  switch (maybeScope) {
-    case Scope.ITEMS_READ:
-    case Scope.COINS_READ:
-    case Scope.IDENTIFY:
-      return true;
-    default:
-      return false;
-  }
+function isValidScope(maybeScope: string): maybeScope is Scope {
+  return new Set(Object.values(Scope)).has(maybeScope as Scope);
 }
