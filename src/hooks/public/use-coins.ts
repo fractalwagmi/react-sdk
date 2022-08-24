2472a5353469143192c00d460f7c86442d4ec161
Kan Adachi <kadachi1023@gmail.com>
2022-08-24 08:52:42 -0700

    Renames `FractalError` to `FractalSDKError`

--- src/hooks/public/use-coins.ts
+++ src/hooks/public/use-coins.ts
@@ -2,7 +2,7 @@ import { sdkApiClient } from 'core/api/client';
 import { Endpoint } from 'core/api/endpoints';
 import { maybeIncludeAuthorizationHeaders } from 'core/api/headers';
 import { transformCoins } from 'core/api/transformers/coins';
-import { FractalError } from 'core/error';
+import { FractalSDKError } from 'core/error';
 import { maybeGetAccessToken } from 'core/token';
 import { PublicHookResponse } from 'hooks/public/types';
 import { useUser } from 'hooks/public/use-user';
@@ -37,10 +37,10 @@ export const useCoins = (): PublicHookResponse<Coin[]> => {
 
   const coins = useMemo(() => transformCoins(data?.coins ?? []), [data?.coins]);
 
-  let error: FractalError | undefined;
+  let error: FractalSDKError | undefined;
   if (errorResponse) {
     // TODO(ENG-394): Enumerate possible errors.
-    error = new FractalError('Unable to retrieve coins');
+    error = new FractalSDKError('Unable to retrieve coins');
   }
 
   return {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import { FractalSDKError } from 'core/error';
+
 export { SignIn as SignInWithFractal } from 'components/sign-in';
 export { UserContextProvider as FractalProvider } from 'context/user';
 export type { SignInProps } from 'components/sign-in';
 
-export { FractalError } from 'core/error';
+export { FractalSDKError } from 'core/error';
 export { Scope } from 'types';
 export type { User, UserWallet, Coin, Item } from 'types';
 
+// TODO: Remove FractalError in next major version, maintaining for backwards
+// compatibility.
+/**
+ * @deprecated Use `FractalSDKError`. The `FractalError` alias will be removed
+ *   in the next version.
+ */
+export const FractalError = FractalSDKError;
 export * from 'hooks/public';
