657400e8a676f473867f41539d81c9843233c9fb
Kan Adachi <kadachi1023@gmail.com>
2022-09-09 06:42:40 -0700

    Adds `useBuyItem` hook

---
+++ src/hooks/public/use-buy-item.ts
@@ -0,0 +1,54 @@
+import {
+  FractalSDKError,
+  FractalSDKSignTransactionUnknownError,
+} from 'core/error';
+import { useSignTransaction } from 'hooks';
+import { useGenerateBuyTransactionMutation } from 'queries/items';
+import { useCallback } from 'react';
+
+export interface BuyItemParameters {
+  /**
+   * The quantity of `tokenId`s to purchase.
+   *
+   * Defaults to 1. (This should be 1 for all NFTs).
+   */
+  quantity?: number;
+
+  /** The token address of the item being purchased. */
+  tokenId: string;
+}
+
+export const useBuyItem = () => {
+  const { mutateAsync: generateBuyTransaction } =
+    useGenerateBuyTransactionMutation();
+  const { signTransaction } = useSignTransaction();
+
+  const buyItem = useCallback(
+    async ({
+      quantity = 1,
+      tokenId,
+    }: BuyItemParameters): Promise<{ signature: string }> => {
+      try {
+        const { transaction } = await generateBuyTransaction({
+          quantity,
+          tokenId,
+        });
+        const { signature } = await signTransaction(transaction);
+        return { signature };
+      } catch (err: unknown) {
+        if (err instanceof FractalSDKError) {
+          throw err;
+        }
+        // TODO: Enumerate all possible errors for any grpc errors that come
+        // from `generateBuyTransaction`.
+        throw new FractalSDKSignTransactionUnknownError(
+          `An unknown error occured while attempting to buy ${tokenId}. ` +
+            `err = ${err}`,
+        );
+      }
+    },
+    [],
+  );
+
+  return { buyItem };
+};
--- src/queries/items.ts
+++ src/queries/items.ts
@@ -1,14 +1,38 @@
-import { FractalSdkWalletGetItemsResponse } from '@fractalwagmi/ts-api';
-import { useQuery } from '@tanstack/react-query';
+import {
+  FractalSdkMarketplaceGetTokenBuyTransactionResponse,
+  FractalSdkWalletGetItemsResponse,
+} from '@fractalwagmi/ts-api';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { sdkApiClient } from 'core/api/client';
 import { ApiFeature } from 'core/api/types';
+import { FractalSDKAuthenticationError } from 'core/error';
 import { useUser } from 'hooks/public/use-user';
+import { useUserWallet } from 'hooks/public/use-user-wallet';
 
 enum ItemApiKey {
+  GENERATE_BUY_TRANSACTION = 'GENERATE_BUY_TRANSACTION',
   GET_ITEMS = 'GET_ITEMS',
 }
 
+interface GenerateBuyTransactionParameters {
+  quantity: number;
+  tokenId: string;
+  walletId: string;
+}
+
 export const ItemApiKeys = {
+  generateBuyTransaction: ({
+    quantity,
+    tokenId,
+    walletId,
+  }: Partial<GenerateBuyTransactionParameters>) =>
+    [
+      ApiFeature.ITEMS,
+      ItemApiKey.GENERATE_BUY_TRANSACTION,
+      tokenId,
+      walletId,
+      quantity,
+    ] as const,
   getItems: (userId: string | undefined) =>
     [ApiFeature.ITEMS, ItemApiKey.GET_ITEMS, userId] as const,
 };
@@ -24,7 +48,31 @@ export const useGetItemsQuery = () => {
   );
 };
 
+export const useGenerateBuyTransactionMutation = () => {
+  const { data: userWallet } = useUserWallet();
+  return useMutation(
+    async ({
+      quantity,
+      tokenId,
+    }: Omit<GenerateBuyTransactionParameters, 'walletId'>) => {
+      const walletId = userWallet?.solanaPublicKeys[0];
+      if (!walletId) {
+        throw new FractalSDKAuthenticationError(
+          'Missing wallet address.' +
+            `Expected a non-empty string but received ${walletId}`,
+        );
+      }
+      return generateBuyTransaction({
+        quantity,
+        tokenId,
+        walletId,
+      });
+    },
+  );
+};
+
 const CoinApi = {
+  generateBuyTransaction,
   getItems,
 };
 
@@ -33,4 +81,19 @@ async function getItems(): Promise<FractalSdkWalletGetItemsResponse> {
   // anything.
   const result = (await sdkApiClient.v1.getWalletItems()).data;
   return result;
+}
+
+async function generateBuyTransaction({
+  quantity,
+  tokenId,
+  walletId,
+}: GenerateBuyTransactionParameters): Promise<FractalSdkMarketplaceGetTokenBuyTransactionResponse> {
+  // TODO: Update to throw a FractalSDKError instance instead of throwing
+  // anything.
+  const result = await sdkApiClient.v1.tokenBuyTransaction({
+    quantity,
+    tokenId,
+    walletId,
+  });
+  return result.data;
 }
