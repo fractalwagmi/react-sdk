d844def1edc8e40dab25239594daaf128a153da4
Dima Ivanyuk <dimaivanyuk@gmail.com>
2023-01-19 16:49:36 -0500

    default onramp window dims

--- src/hooks/public/use-onramp.ts
+++ src/hooks/public/use-onramp.ts
@@ -3,15 +3,11 @@ import { FractalSDKContext } from 'context/fractal-sdk-context';
 import { useCallback, useContext } from 'react';
 
 const ONRAMP_URL = 'https://fractal.is/onramp';
-const POPUP_HEIGHT_PX = 600;
+const POPUP_HEIGHT_PX = 672;
 
 export const useOnramp = () => {
   const { clientId, user } = useContext(FractalSDKContext);
-  const {
-    // close: closePopup,
-    // connection,
-    open: openPopup,
-  } = usePopupConnection({
+  const { open: openPopup } = usePopupConnection({
     enabled: !user,
     heightPx: POPUP_HEIGHT_PX,
     platform: Platform.REACT_SDK,
