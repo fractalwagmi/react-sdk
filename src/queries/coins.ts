7d34d38c0139610e4bd701285d8a45bca52d2a94
Kan Adachi <kadachi1023@gmail.com>
2023-02-13 08:28:34 +0900

    Updates API packages to bring in new types

--- src/queries/coins.ts
+++ src/queries/coins.ts
@@ -1,5 +1,6 @@
 import { FractalSdkWalletGetCoinsResponse } from '@fractalwagmi/fractal-sdk-public-api';
 import { useQuery } from '@tanstack/react-query';
+
 import { sdkApiClient } from 'core/api/client';
 import { ApiFeature } from 'core/api/types';
 import { FractalSDKGetCoinsUnknownError } from 'core/error';
