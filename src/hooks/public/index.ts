8a07395fdfc3838af5b7677ec92b096c9da92512
Kan Adachi <kadachi1023@gmail.com>
2023-05-19 15:44:50 +0900

    Exports `useSignMessage`

--- src/hooks/public/index.ts
+++ src/hooks/public/index.ts
@@ -8,8 +8,10 @@ export * from 'hooks/public/use-list-item';
 export * from 'hooks/public/use-sign-out';
 export * from 'hooks/public/use-sign-out';
 export * from 'hooks/public/use-sign-transaction';
+export * from 'hooks/public/use-sign-message';
 export * from 'hooks/public/use-user-wallet';
 export * from 'hooks/public/use-user';
 export * from 'hooks/public/use-transaction-status';
 export * from 'hooks/public/use-wait-for-transaction-status';
 export * from 'hooks/public/use-onramp';
+
